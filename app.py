5b6c7bb00dbc129ce1fa9c52f6483dde253c66fa
Michał Jaroszewicz <michjar.2002@gmail.com>
2024-02-26 21:19:38 +0100

    parameterization

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
+from datetime import datetime
+
 USERNAME = "kdrozd"
 PASSWORD = "Death5142"
 # Import modules
 import copernicusmarine
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/weather')
 def root():  # put application's code here
+    latitude_start = request.args.get('latitude_start')
+    latitude_end = request.args.get('latitude_end')
+    logitude_start = request.args.get('logitude_start')
+    logitude_end = request.args.get('logitude_end')
+    time_start = datetime.fromtimestamp(int(request.args.get('time_start'))).date()
+    time_end = datetime.fromtimestamp(int(request.args.get('time_end'))).date()
+
     # Set parameters
     data_request = {
         "dataset_id_sst_gap_l3s": "cmems_obs-sst_atl_phy_nrt_l3s_P1D-m",
-        "longitude": [-6.17, -5.09],
-        "latitude": [35.75, 36.29],
-        "time": ["2023-01-01", "2023-01-31"],
+        "longitude": [float(logitude_start), float(logitude_end)],
+        "latitude": [float(latitude_start), float(latitude_end)],
+        "time": [str(time_start), str(time_end)],
         "variables": ["sea_surface_temperature"]
     }
 
@@ -30,7 +39,6 @@ def root():  # put application's code here
         variables=data_request["variables"], username=USERNAME, password=PASSWORD
     )
     return sst_l3s.to_dict()
-
 
 
 if __name__ == '__main__':
