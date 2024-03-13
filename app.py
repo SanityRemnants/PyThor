74f1b8ad099c93e35a4f8b07e9248c807f3b9aaa
Konrad Drozd <konraddrozd02@gmail.com>
2024-03-13 18:23:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,29 @@ wave_variables_dict = {"significant_wave_height": "VHM0",  # cmems_mod_glo_wav_a
                        "wave_direction": "VMDR",  # cmems_mod_glo_wav_anfc_0.083deg_PT3H-i
                        "wave_period": "nic"}  # cmems_mod_glo_wav_anfc_0.083deg_PT3H-i
 
+
 def map_hour(hour):
-    forecast_hours = {(0, 5): "00", (6, 11): "06", (12, 17): "12", (18, 23): "18"}
+    forecast_hours = {(0, 5): "00", (6, 11): "06",
+                      (12, 17): "12", (18, 23): "18"}
     for key in forecast_hours:
         if key[0] <= hour <= key[1]:
             return forecast_hours[key]
 
-# "wind_direction":"nic",
-# "wind_speed":"nic",
+
+wave_dict = {
+    "wave_direction": "dirpw",
+    "wave_height": "swh",
+    "wave_period": "perpw"
+}
+wind_dict = {
+    "wind_direction": "wdir",
+    "wind_speed": "ws"
+}
 curr_variables = ["sea_current_speed", "sea_current_direction"]
 curr_variables_names = ["eastward_sea_water_velocity",
                         "northward_sea_water_velocity"]  # ,  #cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i
-tide_variables_dict = {"tide_height": "zos"}  # cmems_mod_glo_phy_anfc_0.083deg_PT1H-m
+# cmems_mod_glo_phy_anfc_0.083deg_PT1H-m
+tide_variables_dict = {"tide_height": "zos"}
 
 
 # TODO podmienić nic na aktualne nazwy zmiennych z copernicusa
@@ -98,19 +109,22 @@ def fetch_wave(data, variables):
     forecast_hour = map_hour(now.hour)
 
     url = (
-        "https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs."+now.strftime("%Y%m%d")+"/"+forecast_hour+"/wave/gridded/"
+        "https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs." +
+        now.strftime("%Y%m%d")+"/"+forecast_hour+"/wave/gridded/"
         "gfswave.t"+forecast_hour+"z.global.0p16.f000.grib2"
     )
 
     filename = "ww" + now.strftime("%Y%m%d") + forecast_hour + ".grib2"
     try:
-        urlretrieve(url,filename)
-        wave_unproccessed = xr.load_dataset(filename,engine='cfgrib')
+        urlretrieve(url, filename)
+        wave_unproccessed = xr.load_dataset(filename, engine='cfgrib')
         for v in wave_unproccessed:
-            print("{}, {}, {}".format(v, wave_unproccessed[v].attrs["long_name"], wave_unproccessed[v].attrs["units"]))
+            print("{}, {}, {}".format(
+                v, wave_unproccessed[v].attrs["long_name"], wave_unproccessed[v].attrs["units"]))
         return wave_unproccessed["swh"].values
     except Exception as e:
         return "Exception: " + str(e)
+
 
 def fetch_wind(data, variables):
     now = datetime.now().astimezone(pytz.timezone('America/New_York'))
@@ -118,19 +132,22 @@ def fetch_wind(data, variables):
     forecast_hour = map_hour(now.hour)
 
     url = (
-        "https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs."+now.strftime("%Y%m%d")+"/"+forecast_hour+"/wave/gridded/"
+        "https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs." +
+        now.strftime("%Y%m%d")+"/"+forecast_hour+"/wave/gridded/"
         "gfswave.t"+forecast_hour+"z.global.0p16.f000.grib2"
     )
-    filename = "ww" +now.strftime("%Y%m%d")+ forecast_hour +".grib2"
+    filename = "ww" + now.strftime("%Y%m%d") + forecast_hour + ".grib2"
     try:
-        urlretrieve(url,filename)
-        wind_unproccessed = xr.load_dataset(filename,engine='cfgrib')
+        urlretrieve(url, filename)
+        wind_unproccessed = xr.load_dataset(filename, engine='cfgrib')
         # TODO tu na pewno nie ma być waves.grib2 ale nie do końca łapię jak to działa, więc na razie jest jak w fetch_wave + w urlretrieve też coś chuba w "" powinno być
         for v in wind_unproccessed:
-            print("{}, {}, {}".format(v, wind_unproccessed[v].attrs["long_name"], wind_unproccessed[v].attrs["units"]))
+            print("{}, {}, {}".format(
+                v, wind_unproccessed[v].attrs["long_name"], wind_unproccessed[v].attrs["units"]))
         return wind_unproccessed["u"].values
     except Exception as e:
         return "Exception: " + str(e)
+
 
 # Import modules
 app = Flask(__name__)
@@ -143,7 +160,8 @@ def root():  # put application's code here
     logitude_start = request.args.get('logitude_start')
     logitude_end = request.args.get('logitude_end')
     interval = request.args.get('interval', 2)
-    request_vars = request.args.get('variables', "").replace(" ", "").split(",")
+    request_vars = request.args.get(
+        'variables', "").replace(" ", "").split(",")
     epoch_time_start = int(request.args.get('time_start'))
     epoch_time_end = int(request.args.get('time_end'))
     if not latitude_start or not latitude_end or not logitude_start or not logitude_end or not epoch_time_start or not epoch_time_end or len(
@@ -177,9 +195,9 @@ def root():  # put application's code here
 
 
 if __name__ == '__main__':
-    #_ = copernicusmarine.open_dataset(dataset_id="cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i", username=USERNAME,
+    # _ = copernicusmarine.open_dataset(dataset_id="cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i", username=USERNAME,
     #                                  password=PASSWORD)
-    #_ = copernicusmarine.open_dataset(dataset_id="cmems_mod_glo_phy_anfc_0.083deg_PT1H-m", username=USERNAME,
+    # _ = copernicusmarine.open_dataset(dataset_id="cmems_mod_glo_phy_anfc_0.083deg_PT1H-m", username=USERNAME,
     #                                  password=PASSWORD)
-    print(fetch_wind(0,0))
-    #app.run()
+    print(fetch_wave(0, 0))
+    # app.run()
