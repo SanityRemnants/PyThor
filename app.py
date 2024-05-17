bcb37854ab60575164bc25f352b48028756e2d1d
Michał Jaroszewicz <michjar.2002@gmail.com>
2024-05-18 00:15:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-import json
 from copy import deepcopy
 import numpy as np
-import scipy.interpolate
 from flask import Flask, request, Response
 import copernicusmarine
-from scipy.interpolate import griddata, SmoothSphereBivariateSpline, RegularGridInterpolator, Rbf
+from scipy.interpolate import RegularGridInterpolator, Rbf
 
 from data_request import DataRequest
 from fetcher import Fetcher
-import scipy.interpolate as sci
 
 import yaml
 
-USERNAME = ""
-PASSWORD = ""
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
     USERNAME = config["coppernicus_acount"]["username"]
     PASSWORD = config["coppernicus_acount"]["password"]
 
-# Import modules
 app = Flask(__name__)
 
 
@@ -43,14 +37,11 @@ def root():  # put application's code here
         return Response(status=400)
     result = Fetcher(data_request).fetch()
 
-    # return result
-
     wave_wind_not_inter = result["waves_and_wind"]
     lat = np.array(wave_wind_not_inter["latitude"])
     lon = np.array(wave_wind_not_inter["longitude"])
     time = np.array(wave_wind_not_inter["time"])
 
-    coords = data_request.get_coordinates()
     lat_inter = np.arange(lat[0], lat[-1], 0.1)
     lon_inter = np.arange(lon[0], lon[-1], 0.1)
     if time[0] != time[-1]:
@@ -61,7 +52,6 @@ def root():  # put application's code here
     keys_to_check = ["dirpw", "swh", "perpw", "u", "v", "ws"]
     keys = []
     weather = {}
-    normal = {}
     for key in keys_to_check:
         if key in wave_wind_not_inter:
             keys.append(key)
@@ -78,36 +68,21 @@ def root():  # put application's code here
     normal = {key: value.tolist() if isinstance(value, np.ndarray) else value for key, value in
               weather.items()}
 
-    lat_rad = np.deg2rad(lat)
-    lon_rad = np.deg2rad(lon)
-    lon_rad = np.where(lon_rad < 0, lon_rad + 2 * np.pi, lon_rad)
-
-    lon_rad_inter = np.deg2rad(lon_inter)
-    lat_rad_inter = np.deg2rad(lat_inter)
-    lon_rad_inter = np.where(lon_rad_inter < 0, lon_rad_inter + 2 * np.pi, lon_rad_inter)
-
     lon_grid, lat_grid = np.meshgrid(lon, lat)
     lon_inter_grid, lat_inter_grid = np.meshgrid(lon_inter, lat_inter)
     res = [[[0] * len(lon_inter) for _ in range(len(lat_inter))] for _ in range(len(time))]
     for key in keys:
         for k in range(len(time)):
+            # Interpolacja po długości i szerokości
             elem = weather[key]
             nan_mask = np.isnan(elem[k])
             indices = np.where(~nan_mask)
-            nan_mask_flat = nan_mask.ravel()
-
             data_points_valid = elem[k][indices]
             lat_valid = lat_grid[~nan_mask]
             lon_valid = lon_grid[~nan_mask]
             x, y, z = spherical_to_cartesian(lat_valid.ravel(), lon_valid.ravel())
-
-
-            nxest = 2 * len(np.unique(lon_rad))  # Na przykład podwojenie liczby unikalnych wartości długości
-            nyest = 2 * len(np.unique(lat_rad))
             interp_spatial = Rbf(x, y, z, data_points_valid, function='thin_plate', smooth=0)
 
-            #interp_spatial = SmoothSphereBivariateSpline(lat_rad_valid.ravel(), lon_rad_valid.ravel(),
-            #                                             data_points_valid)
             x_inter, y_inter, z_inter = spherical_to_cartesian(lat_inter_grid, lon_inter_grid)
             res[k] = interp_spatial(x_inter, y_inter, z_inter)
 
@@ -140,27 +115,7 @@ def root():  # put application's code here
     weather["lon_inter"] = lon_inter.tolist()
 
     return weather
-    '''
-    
-    lon_min = np.array(wave_wind_not_inter["longitude"])[0]
-    lon_max = np.array(wave_wind_not_inter["longitude"])[-1]
-    lat_min = np.array(wave_wind_not_inter["latitude"])[0]
-    lat_max = np.array(wave_wind_not_inter["latitude"])[-1]
-    time_min = np.array(wave_wind_not_inter["time"])[0]
-    time_max = np.array(wave_wind_not_inter["time"])[-1]
 
-    dirpw = np.array(wave_wind_not_inter["dirpw"])
-    maska = np.isnan(dirpw)
-    punkty = np.argwhere(~maska)
-    wartosci = dirpw[~maska]
-    nowa_rozdzielczosc = 2
-    z_interp, y_interp, x_interp = np.meshgrid(
-        np.linspace(time_min, time_max, int(nowa_rozdzielczosc * (time_max - time_min) + 1)),
-        np.linspace(lat_min, lat_max, int(nowa_rozdzielczosc * (lat_max - lat_min) + 1)),
-        np.linspace(lon_min, lon_max, int(nowa_rozdzielczosc * (lon_max - lon_min) + 1)))
-
-    dane_zinterpolowane = griddata(punkty, wartosci, (z_interp, y_interp, x_interp), method='linear')
-    return dane_zinterpolowane'''
 
 
 if __name__ == '__main__':
