a7234e666eca3bb956ff073907c4f5808a556ac5
Michał Jaroszewicz <118765484+Blu3berryJam@users.noreply.github.com>
2024-10-02 11:46:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask, request, Response
 
 from data_request import DataRequest
 from fetcher import Fetcher
-from interpolation import interpolate, interpolate_for_copernicus
+from interpolation import interpolate
+import json
 
 import yaml
 
@@ -21,16 +24,23 @@ def root():
                                request.args.get('time_start'), request.args.get('time_end'),
                                request.args.get('interval', 60),
                                request.args.get('variables', "").replace(" ", "").split(","))
-    time = [int(request.args.get('time_start')), int(request.args.get('time_end'))]
-    if not data_request.is_valid():
-        return Response(status=400)
-    result = Fetcher(data_request).fetch()
+    file_name = str(data_request)
+    if not os.path.isfile(f'data/{file_name}.json'):
+        time = [int(request.args.get('time_start')), int(request.args.get('time_end'))]
+        if not data_request.is_valid():
+            return Response(status=400)
+        result = Fetcher(data_request).fetch()
 
-    res = interpolate(result, data_request, time)
+        res = interpolate(result, data_request, time)
 
-    return res
+        with open(f'data/{file_name}.json', 'w') as f:
+            json.dump(res, f)
+        return res
+    else:
+        with open(f'data/{file_name}.json', 'r') as f:
+            return f
+
 
 
 if __name__ == '__main__':
     app.run()
-# print(fetch_wave(0, 0))
